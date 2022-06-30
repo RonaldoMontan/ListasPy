13fdb90ac0ffc1ba9411b25031790a51282158fe
Ronaldo <ronaldogithub@gmail.com>
2022-06-30 09:08:18 -0300

    the last commit made was incomplete

--- lista2.py
+++ lista2.py
@@ -1,13 +1,16 @@
 lista = []
-controle = ''
 
+control = ''
+length = 0
 
+while control in 'yY':
 
-while controle in 'yY':
-    lista.append(int(input("Informe o valor para adicionar: ")))
-    for posição in range(len(lista)):
-        print()     
-
-
-
-    controle = str(input("Deseja continuar ? (Y/N): "))
+    add = int(input("enter any values: "))
+    if add not in lista:
+        lista.append(add)  
+    else:        
+        print(f'Sorry, the value {add} already exists in the list' )
+            
+    control = str(input("want to continue ? [Y/N] "))
+    length += 1
+print(lista)  
