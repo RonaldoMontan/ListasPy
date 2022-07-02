3440fc29748264546a9986391ea7e2e3d5a3948b
RonaldoMontan <106325337+RonaldoMontan@users.noreply.github.com>
2022-07-02 14:56:06 -0300

    Update lista3.py

--- lista3.py
+++ lista3.py
@@ -21,3 +21,26 @@ while len(lista) != 5:
     print(lista)           
 
 print(lista)
+
+#solução mais precisa foi esta
+
+lista = []
+
+for indice in range(0, 10):
+    value = int(input('Qual valor deseja inserir ?'))
+
+    #inserimos o primeiro valor ou o maior valor
+    if indice == 0 or value > lista[-1]:
+        lista.append(value)
+        print(lista)
+
+    else:
+        posição_da_varredura = 0 #aqui é o segredo para a varredura das posições
+        while posição_da_varredura < len(lista):
+            if value <= lista[posição_da_varredura]:
+                lista.insert(posição_da_varredura, value)
+                print(lista)
+                break #para inserir uma vez o valor
+            posição_da_varredura += 1 #este incremento tem impacto na linha 13 (while)   
+            
+print(lista)
