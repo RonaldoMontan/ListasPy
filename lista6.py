63c92a48cb63a43e63fb659974d835dbffc13475
Ronaldo Montan <ronaldogithub@gmail.com>
2022-07-04 23:03:01 -0300

    inserindo lista dentro de outra lista e manipulando seu conteudo

--- lista6.py
+++ lista6.py
@@ -1,13 +1,33 @@
-lista = ''
+todos = []
+dados = []
+controle = ''
+maior = menor = 0
 
-#lista.append(str(input('Digite sua expressão: ')))
-lista = str(input('Digite sua expressão '))
+while controle in 'sS':
 
-n = lista.count('(')
-m = lista.count(')')
+    dados.append(str(input("Nome: ")))
+    dados.append(float(input("Peso: ")))    
+    #DESCOBRINDO MAIOR E MENOR
+    if len(todos) == 0:
+        maior = menor = dados[1] #o primeiro valor ja será atrinuido pois o primeiro valor pode ser o menor ou o maior 
+    else:
+        if dados[1] > maior:
+            maior = dados[1]
+        if dados[1] < menor:
+            menor = dados[1]   
+    todos.append(dados[:])#faz uma copia das informações para todos
+    dados.clear()#limpa os dados para inicirar uma nova pu seja os indices vão iniciar no zero  
+    controle = str(input("Deseja continuar ?"))[0]#qualquer valor diferente de Ss sai do loop
 
-if n == m :
-    print('Sua expressão é valida')
-else:
-    print('Sua expressão não é valida')    
+print(f'a quantidade de pessoas registradas foi {len(todos)}')
 
+for pessoas in todos:
+    if pessoas[1] == maior:
+        print(f'[{pessoas[0]}] tem o maior peso de {maior}')
+        #pessoas se refere ao conjunto de dados nome e peso
+        #pessoas[1] se refere ao peso por isso ele compara
+for pessoas in todos:
+    if pessoas[1] == menor:
+        print(f'[{pessoas[0]}] tem o menor peso de {menor}')
+        #pessoas se refere ao conjunto de dados nome e peso
+        #pessoas[1] se refere ao peso por isso ele compara
