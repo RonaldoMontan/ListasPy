fdf0bdadb8586ccc443dc657dd50410d27ebcf67
Ronaldo Montan <ronaldogithub@gmail.com>
2022-07-05 22:55:45 -0300

    outra maneira de criar uma lista e adicionar outra lista dentro

--- lista7.py
+++ lista7.py
@@ -19,3 +19,21 @@ listatotal.append(impares[:])
 print(listatotal)
 print(f'lista pares {listatotal[0]}')
 print(f'Lista Impares {listatotal[1]}')
+
+#---Outra maneira de fazer---
+
+lista = [[], []]
+
+for w in range(0, 7):
+    numero = int(input('digite seu valor: '))
+    if numero %2 == 0:
+        lista[0].append(numero)
+    else:
+        lista[1].append(numero)
+
+#ordedando os valores para crescente da lista interna
+lista[0].sort()
+lista[1].sort()
+
+print(f"Valores pares da lista {lista[0]}")
+print(f"Valores impares da lista {lista[1]}")
