58ae47b28faa99db64f6c0e4186e2da035750ba2
Ronaldo <ronaldogithub@gmail.com>
2022-07-07 14:38:44 -0300

    Conhecendo Matriz no python

--- lista8.py
+++ lista8.py
@@ -1,12 +1,12 @@
-matriz = [[0,0,0], [0,0,0], [0,0,0]]
+matriz = [[0,0,0], [0,0,0], [0,0,0]] #será uma matriz 3x3
 
-for c in range(0,3):    
-    for l in range(0, 3):
-        matriz[c][l] = int(input(f"Guarde seu valor aqui! [{c},{l}] "))
+#for para preenchimento
+for coluna in range(0,3):    
+    for linha in range(0, 3):
+        matriz[coluna][linha] = int(input(f"Guarde seu valor aqui! [{coluna},{linha}] "))
 
-
-for c in range(0, 3):
-    for l in range(0, 3):
-        print(f'[{matriz[c][l]:^5}]', end='')#  :^5 serve para centralizar
-    print()    
-
+#for para leitura
+for coluna in range(0, 3):
+    for linha in range(0, 3):
+        print(f'[{matriz[coluna][linha]:^5}]', end='')#  :^5 serve para centralizar o resultado
+    print() #fez a primeira linha quebra para a segunda, aqui fica o efeito 3x3   
