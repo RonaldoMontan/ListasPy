82a6098f28c0bb85ce4b61cb919def32e5554f35
Ronaldo Montan <ronaldogithub@gmail.com>
2022-07-06 23:57:29 -0300

    Conhecendo Matriz no python

---
+++ lista8.py
@@ -0,0 +1,12 @@
+matriz = [[0,0,0], [0,0,0], [0,0,0]]
+
+for c in range(0,3):    
+    for l in range(0, 3):
+        matriz[c][l] = int(input("Guarde seu valor aqui! {[c][l]} "))
+
+
+for c in range(0, 3):
+    for l in range(0, 3):
+        print(f'[{matriz[c][l]:^5}]', end='')#  :^5 serve para centralizar
+    print()    
+
