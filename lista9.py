31b6385f2cf4ddbd060b7fe9e112e8345c936ae5
Ronaldo <ronaldogithub@gmail.com>
2022-07-08 09:42:20 -0300

    update, programa completo

--- lista9.py
+++ lista9.py
@@ -1,32 +1,19 @@
 matriz = [[0,0,0], [0,0,0], [0,0,0]]
 pares = somaTerceiraColuna = 0
 maior = 0
+
 #preenchimento
 for coluna in range(0, 3):
     for linha in range(0, 3):
         matriz[coluna][linha] = int(input(f"guarde seu valor aui nesta posição: [{coluna} | {linha}]  "))
 
-
 #leitura
-for coluna in range(0, 3):
-    for linha in range(0, 3):
-        
-        #soma valores terceira coluna
-        if (coluna == 0 and linha == 2) or (coluna == 1 and coluna ==2) or (coluna == 2 and linha == 2):
-            somaTerceiraColuna += matriz[coluna][linha]
-           
-        #soma valores pares
-        if matriz[coluna][linha] %2 == 0:
-            pares += matriz[coluna][linha]
+for linha in matriz:#vamos passando linha por linha e a coluna 2 esta fixado abaixo
+    somaTerceiraColuna += linha[2]# aqui se refere aos indices [0][2] - [1][2] - [2][2] ou seja linha é o valor que varia
 
-        #MAIOR VALOR DA MATRIZ
-        if coluna == 0 and linha == 0:
-            maior = matriz[coluna][linha]
-            print(maior)
-        else:
-            if matriz[coluna][linha] > maior:
-                maior += matriz[coluna][linha]
-
+    for coluna in linha:#o primeiro for vai passado pela linha e este segundo passando pela coluna, com isso percorre todas as posições
+        if coluna %2 == 0:
+            pares += coluna
 
 #impressão
 for coluna in range(0, 3):
@@ -35,6 +22,6 @@ for coluna in range(0, 3):
     print()
 
 
-print(pares)
-print(somaTerceiraColuna)
-print(maior)
+print(f'A soma de todos os valores pares da matriz é   {pares}')
+print(f'A Soma da terceira coluna é   {somaTerceiraColuna}')
+print(f'O maio valor da segunda linha é   {max(matriz[1])}') 
