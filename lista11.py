57e464298f7aada4f33351a6ac62bc32e0cb07d5
Ronaldo Montan <ronaldogithub@gmail.com>
2022-07-11 22:40:54 -0300

    manipulação de lista mais precisa

--- lista11.py
+++ lista11.py
@@ -2,16 +2,33 @@ lista = []
 listaParaCopia = list()
 
 while True:
+    listaParaCopia.append(str(input(">> Nome do aluno: ")))
+    listaParaCopia.append(float(input(">> Nota 1:  ")))
+    listaParaCopia.append(float(input(">> Nota 2:  ")))
+    listaParaCopia.append((listaParaCopia[1] + listaParaCopia[2])/2)#armazena a media na posição 3
+    lista.append(listaParaCopia[:])#momento para copia das informações
+    listaParaCopia.clear()#esvazia a lista para receber novos dados
 
-    lista.append(str(input(">> Nome do aluno: ")))
-    lista.append(float(input(">> Nota 1:  ")))
-    lista.append(float(input(">> Nota 2:  ")))
-    
-
-
+    #controle do loop
     resposta = str(input("Deseja continuar?  "))[0]
     if resposta in 'Nn':
         break
+print()
+print('>>> Saida das informações <<<')
+print('_____________________________')
+print('Nº     Nome             Media')
 
+for i in range(0, len(lista)):
+    print(f'{i:<4}--  {lista[i][0]:<10} {lista[i][3]:>8.1f} ')
 
-print(lista)
+while True: 
+    resposta = int(input('Digite o numero do aluno para ver mais detalhes ou 999 para sair: '))
+
+    for i in range(0, len(lista)):
+        if resposta == i:
+            print(f'O(A) aluno(a) {lista[resposta][0]} teve   {lista[resposta][1]}  |  {lista[resposta][2]}')
+            print()    
+    if resposta == 999:
+        break
+    
+print('Saindo...')    
